265c4036dee85bab86eb972525da068b3505f984
Sayed Farhad Nabizada <nzfarhad@gmail.com>
2023-09-08 10:31:26 +0500

    modified functions

---
+++ function/gis_functions_2.R
@@ -0,0 +1,170 @@
+# function to filter layers
+
+library(raster)
+library(sf)
+
+# Provincial filter
+filter_province <- function(settlements, settlements_boundry_4326, af_buildings, raster,  province){
+  settlements <- settlements %>% filter(Prvnce_Eng %in% province)
+  settlements_boundry_4326 <- settlements_boundry_4326 %>% 
+    filter(Prvnce_Eng %in% province)
+  
+  af_buildings_filtered <- af_buildings %>% 
+    st_filter(settlements_boundry_4326, .predicate = st_intersects)
+  
+  cropped_raster <- raster::crop(raster, extent(settlements_boundry_4326))
+  masked_raster <- raster::mask(cropped_raster, settlements_boundry_4326)
+  
+  output_list <- list(settlements_filt = settlements,
+                      settlements_boundry_filt = settlements_boundry_4326,
+                      af_buildings_filt = af_buildings_filtered,
+                      population_raster = masked_raster
+  )
+  return(output_list)
+  
+}
+
+
+# District filter
+filter_district <- function(settlements, settlements_boundry_4326, af_buildings, raster, district){
+  
+  settlements <- settlements %>% filter(Dstrct_Eng %in% district)
+  settlements_boundry_4326 <- settlements_boundry_4326 %>% 
+    filter(Dstrct_Eng %in% district)
+  
+  af_buildings_filtered <- af_buildings %>% 
+    st_filter(settlements_boundry_4326, .predicate = st_intersects)
+  
+  cropped_raster <- raster::crop(raster, extent(settlements_boundry_4326))
+  masked_raster <- raster::mask(cropped_raster, settlements_boundry_4326)
+  
+  output_list <- list(settlements_filt = settlements,
+                      settlements_boundry_filt = settlements_boundry_4326,
+                      af_buildings_filt = af_buildings_filtered,
+                      population_raster = masked_raster
+  )
+  return(output_list)
+  
+}
+
+
+
+# Settlement filter
+
+filter_settlment <- function(settlements, settlements_boundry, af_buildings, raster1, my_id){
+  
+  settlements <- settlements %>% filter(Ref_ID %in% my_id)
+  settlements_boundry <- settlements_boundry %>% 
+    filter(Ref_ID %in% my_id)
+  
+  # settlements_boundry_32642 <- st_transform(settlements_boundry, crs = st_crs(32642))
+  # af_buildings <- st_transform(af_buildings, crs = st_crs(32642))
+  
+  af_buildings_filtered <- af_buildings %>% 
+    st_filter(settlements_boundry, .predicate = st_intersects)
+  
+  settlements_boundry_4326 <- settlements_boundry %>% st_transform(crs = 4326)
+  
+  settlements_boundry_4326 <- settlements_boundry_4326 %>% st_transform(crs = 4326)
+  cropped_raster <- raster::crop(raster1, extent(settlements_boundry_4326))
+  masked_raster <- raster::mask(cropped_raster, settlements_boundry_4326)
+  
+  output_list <- list(settlements_filt = settlements,
+                      settlements_boundry_filt = settlements_boundry,
+                      af_buildings_filt = af_buildings_filtered,
+                      population_raster = masked_raster
+  )
+  return(output_list)
+  
+}
+
+
+# filter_settlment_32642 <- function(settlements, settlements_boundry, af_buildings, raster1, my_id){
+#   
+#   settlements <- settlements %>% filter(Ref_ID %in% my_id)
+#   settlements_boundry <- settlements_boundry %>% 
+#     filter(Ref_ID %in% my_id)
+#   
+#   settlements_boundry_32642 <- st_transform(settlements_boundry, crs = st_crs(32642))
+#   af_buildings <- st_transform(af_buildings, crs = st_crs(32642))
+#   
+#   af_buildings_filtered <- af_buildings %>% 
+#     st_filter(settlements_boundry_32642, .predicate = st_intersects)
+#   
+#   # settlements_boundry_4326 <- settlements_boundry_32642 %>% st_transform(crs = 4326)
+#   
+#   # settlements_boundry_4326 <- settlements_boundry_4326 %>% st_transform(crs = 4326)
+#   cropped_raster <- raster::crop(raster1, extent(settlements_boundry_32642))
+#   masked_raster <- raster::mask(cropped_raster, settlements_boundry_32642)
+#   
+#   output_list <- list(settlements_filt = settlements,
+#                       settlements_boundry_filt = settlements_boundry_32642,
+#                       af_buildings_filt = af_buildings_filtered,
+#                       population_raster = masked_raster
+#   )
+#   return(output_list)
+#   
+# }
+
+
+calculate_population <- function(raster){
+  population <- raster::cellStats(raster, sum, na.rm = TRUE)
+  return(population)
+  print(population)
+}
+
+
+
+# Create Grid - crs Web Mercator (EPSG:3857)
+make_grid <- function(poly, cell_size = 100, my_crs = 3857) {
+  # planar coordinates for Afghanistan EPSG:32642 and ESPG32641
+  my_polygon <- st_transform(poly, crs = st_crs(my_crs))
+  
+  # Create the grid
+  grid <- st_make_grid(my_polygon, cellsize = cell_size) %>%
+    st_sf %>%
+    st_filter(my_polygon, .predicate = st_intersects)  #%>%
+  # st_transform(crs = 4326)
+  grid$Shape_Length <- round(as.numeric(st_length(grid$geometry)))
+  grid$Shape_Area <- round(as.numeric(st_area(grid$geometry)))
+  return(grid)
+}
+
+
+
+# Count population per grid
+count_ppp <- function(my_grid, my_raster){
+  count <- vector()
+  for (i in 1:nrow(grid)) {
+    cropped_raster <- raster::crop(my_raster, extent(st_sf(my_grid$geometry[i])))
+    masked_raster <- raster::mask(cropped_raster, st_sf(my_grid$geometry[i]))
+    count <- c(count, calculate_population(masked_raster))  
+  }
+  return(count)
+  
+}
+
+
+calculate_building_area_per_grid <- function(my_grid, my_buildings){
+  
+  grid_area <- vector()
+  for (i in 1:nrow(my_grid)) {
+    grid_building_area <- st_crop(my_buildings, my_grid$geometry[i]) %>% suppressMessages()
+    grid_building_area_sum <- round(sum(st_area(grid_building_area)),2) %>% as.numeric()
+    grid_area <- c(grid_area, grid_building_area_sum)
+  }
+  my_grid$area_sqm <- grid_area
+  
+  return(my_grid)
+}
+
+
+
+
+clip_raster <- function(raster, polygon)  {
+  cropped_raster <- raster::crop(raster, extent(polygon))
+  masked_raster <- raster::mask(cropped_raster, polygon)
+  return(masked_raster)
+}
+
+
